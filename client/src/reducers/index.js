96326241e5819c7ccdc0f73a276bf75d3338fdc8
Evan Price <eprice@web.com>
2019-04-28 20:57:06 +0100

    streamsreducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import { reducer as reduxFormReducer } from "redux-form";
 import authReducer from "./authReducer";
+import streamReducer from "./streamReducer";
 
 export default combineReducers({
   auth: authReducer,
-  form: reduxFormReducer
+  form: reduxFormReducer,
+  streams: streamReducer
 });
